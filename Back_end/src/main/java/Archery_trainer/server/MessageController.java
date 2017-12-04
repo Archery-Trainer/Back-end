ae5bde5dd9af77df289659103dcb9237de8f65ec
Timo <jootimo@gmail.com>
2017-12-04 20:06:07 +0200

    Return Json from controller

--- Back_end/src/main/java/Archery_trainer/server/MessageController.java
+++ Back_end/src/main/java/Archery_trainer/server/MessageController.java
@@ -1,9 +1,9 @@
 package Archery_trainer.server;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @EnableAutoConfiguration
@@ -13,11 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 */
 public class MessageController
 {
+	private final AtomicLong counter = new AtomicLong(0);
 
 	@RequestMapping("/message")
-	String getMessage() {
-		Message msg = new Message(0, MessageType.SENSORVALUE, "Heippahei!");
-		return "Moi";
+	Message createAndSendMessage() {
+		Message msg = new Message(counter.addAndGet(1), MessageType.SENSORVALUE, "Heippahei!");
+
+		return msg; //Builds JSON automatically
 	}
 	
 }
