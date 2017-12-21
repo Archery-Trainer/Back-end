9e776625332a82409779cd4fd0fc606cd84c98bf
Timo <jootimo@gmail.com>
2017-12-21 19:49:57 +0200

    Removed debugging crap

--- Back_end/src/main/java/Archery_trainer/server/CreateArcherController.java
+++ Back_end/src/main/java/Archery_trainer/server/CreateArcherController.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -15,12 +14,12 @@ public class CreateArcherController {
 
 
     //Recieve Archer object and print it. Return OK.
-    @RequestMapping("/createArcher")
-    public String createArcher(){//@RequestBody Archer a) {
+    @RequestMapping(value = "/createArcher")
+    public ResponseEntity<Archer> get(@RequestBody Archer a) {
 
-        System.out.println("Recieved: ");// + a.toString());
+        System.out.println("Recieved: " + a.toString());
 
-        return "HWEI";//a.toString();//new ResponseEntity<Archer>(a, HttpStatus.OK);
+        return new ResponseEntity<Archer>(a, HttpStatus.OK);
 
     }
 
