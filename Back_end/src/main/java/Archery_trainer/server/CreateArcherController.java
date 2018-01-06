e1a82085defc552a8f0eaaaba8565edc24e76ecb
EC2 Default User <ec2-user@ip-172-31-87-216.ec2.internal>
2018-01-06 13:22:50 +0000

    Working Archer object creation with JSON

--- Back_end/src/main/java/Archery_trainer/server/CreateArcherController.java
+++ Back_end/src/main/java/Archery_trainer/server/CreateArcherController.java
@@ -13,16 +13,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class CreateArcherController {
 
 
-    //Recieve Archer object and print it. Return OK.
+    //Recieve json, convert it to Archer object and return it
     @RequestMapping(value = "/createArcher", method = RequestMethod.POST)
-    public Archer get(@RequestBody String s) {
+    public String get(@RequestBody String s) {
 
+
+	System.out.println("Recieved createArcher request: " + s);
+
+	//Create Archer object
         Gson gson = new Gson();
         Archer a = gson.fromJson(s, Archer.class);
 
         System.out.println("Created new archer: " + a.toString());
 
-        return a;
+	//response:
+	//@TODO: maybe we should just return an 'OK', but this shows
+	//how to send objects back to app
+	String response = gson.toJson(a, Archer.class);	
+
+	return response;
     }
 
 }
