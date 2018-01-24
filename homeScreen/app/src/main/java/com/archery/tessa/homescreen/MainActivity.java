a8f996c412a63e10c5d56f5c5af36766049d066f
Timo <jootimo@gmail.com>
2018-01-24 17:23:26 +0200

    Added Tessa's changes

--- homeScreen/app/src/main/java/com/archery/tessa/homescreen/MainActivity.java
+++ homeScreen/app/src/main/java/com/archery/tessa/homescreen/MainActivity.java
@@ -1,16 +1,12 @@
 package com.archery.tessa.homescreen;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-
-import com.archery.tessa.homescreen.models.Archer;
-import com.archery.tessa.homescreen.tasks.CreateArcherTask;
-
-import mqttClient.MqttMessageHandler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,9 +15,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button button5 = (Button) findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, SessionActivity.class);
@@ -29,21 +24,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, OptionsActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        /*
-        To listen to the MQTT channel for sensor messages, just create this MqttMessageHandler object.
-        You can get the newest message with the handler's method getNewestMessage()
-        NOTE:   you must add the androidkeystore -file from our google drive's keys and certificates folder
-                to app/src/main/assets. Create this directory if it doesn't exist
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        MqttMessageHandler msgHandler = new MqttMessageHandler(this);
-        */
-
-        /*
-        How to send archer object to server and database
-
-        Archer testArcher = new Archer("Matti", "Ahmatti", -1, 120, 190, false);
-        new CreateArcherTask(testArcher).execute(this);
-        */
 
 }}
