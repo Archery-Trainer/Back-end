c72bcc1ded8bff2933e56be57031779c08ee4dd0
Timo <jootimo@gmail.com>
2018-01-29 18:12:26 +0200

    Removed unused files

--- homeScreen/app/src/main/java/mqttClient/OnMessageCallback.java
+++
@@ -1,8 +0,0 @@
-package mqttClient;
-
-/**
- * An interface for a function wrapper that will be called when the MQTT-client receives a message
- */
-public interface OnMessageCallback {
-    void call(String message);
-}
